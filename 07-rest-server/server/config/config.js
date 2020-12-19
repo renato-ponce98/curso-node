d940acbd77e8525480896a52d85d8aaa645f6fc7
Renato Ponce <69768980+renato-ponce98@users.noreply.github.com>
2020-12-19 17:28:43 -0500

    autenticacion con google

--- 07-rest-server/server/config/config.js
+++ 07-rest-server/server/config/config.js
@@ -17,3 +17,8 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 // SEED de autenticacion
 // =============================
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// =============================
+// Google Client ID
+// =============================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '134063358108-9mcuhopvr1oji3t67hlhpiet91944d8s.apps.googleusercontent.com';
